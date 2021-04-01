b76a5fc90fdb210b31650174fd51e2c3b16295f8
Jake Luecke <jrluecke95@gmail.com>
2021-04-01 12:32:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <form>
+          <h1>World Clock</h1>
+          <Switch>
+            <Route path='/' exact>
+              <Home></Home>
+            </Route>
+            <Route path='/timezones' exact>
+              test2
+            </Route>
+          </Switch>
+        </form>
+      </div>
+    </Router>
+    
   );
 }
 

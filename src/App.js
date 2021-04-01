a2790c490007c35ce9a32fc0d4632750735cf4a0
Jake Luecke <jrluecke95@gmail.com>
2021-04-01 16:02:11 -0400

    clock and timezones are saving

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
+  const [ timezones, setTimezones ] = useState([])
   return (
     <Router>
       <div className="App">
-        <form>
-          <h1>World Clock</h1>
-          <Switch>
-            <Route path='/' exact>
-              <Home></Home>
-            </Route>
-            <Route path='/timezones' exact>
-              test2
-            </Route>
-          </Switch>
-        </form>
+        <h1>World Clock</h1>
+        <Switch>
+          <Route path='/' exact>
+            <Home setTimezones={setTimezones} timezones={timezones}></Home>
+          </Route>
+          <Route path='/timezones' exact>
+            test2
+          </Route>
+        </Switch>
       </div>
     </Router>
     
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import TimezoneSelect from 'react-timezone-select';
+import AnalogClock from 'analog-clock-react';
 
-const Home = () => {
+
+const Home = (props) => {
+  const [ timezone, setTimezone] = useState('');
+  const [ time, setTime ] = useState({ hours: 0, minutes: 0, seconds: 0})
+
+  /**
+   * handle submit for form
+   * @param {Event} e form event
+   */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.setTimezones([...props.timezones, timezone])
+  }
+
+
+  useEffect(() => {
+    // updating clock
+    const updateClock = () => {
+      // get a time string for current time and setting timezone state with it the 
+      // timezone object is a date function built in to js
+      const timeString = new Date().toLocaleString('en-US', { 
+        timeZone: timezone.value 
+      });
+      // updating time state with current date information that is being passed from above and turning it back into an object 
+      // 
+      const dateTime = new Date(timeString);
+      setTime({
+        hours: dateTime.getHours(),
+        minutes: dateTime.getMinutes(),
+        seconds: dateTime.getSeconds(),
+      })
+    }
+    // set up interval and store reference in variable so that we can clear interval
+    const interval = setInterval(() => {
+      updateClock();
+    }, 1000);
+    // clean up function
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timezone])
+
   return (
     <div>
-      this is the home page
+      <form onSubmit={handleSubmit}>
+        <TimezoneSelect value={timezone} onChange={setTimezone}/>
+        <button type="submit">Select Timezone</button>
+      </form>
+      {timezone && <AnalogClock useCustomTime {...time}/>}
+      
     </div>
   )
 }
